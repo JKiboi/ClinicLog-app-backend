47d550af0c107f6d3bd55ee1043b73f4599e04fb
JKiboi <anythingtechke@gmail.com>
2024-07-20 18:18:17 +0300

    new commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Configure CORS to allow requests from your frontend origin
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://cliniclog-app.onrender.com"],
+        origin: ["http://localhost:3000", "https://https://cliniclogs.netlify.app/"],
         methods: "GET,HEAD,PUT,PATCH,UPDATE,POST,DELETE",
         credentials: true,
     })
